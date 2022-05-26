6c332391c19a1b8b4eada46a8ade68ed42eae541
arlechann <dragnov3728@gmail.com>
2022-05-27 01:21:02 +0900

    Use date-fns

--- frontend/app/src/pages/post.tsx
+++ frontend/app/src/pages/post.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent } from 'preact';
+import { format } from 'date-fns';
 import { PageProps } from '../types/page';
 import { Post } from '../types/post';
 
@@ -19,7 +20,7 @@ export const PostPage: FunctionComponent<PostPageProps> = (props: PostPageProps)
       <article>
         <div>{post.title}</div>
         <div>
-          <span>update: {post.lastUpdatedDate}</span><span>posted: {post.createdDate}</span>
+          <span>update: {format(post.lastUpdatedDate, 'yyyy-MM-dd')}, </span><span>posted: {format(post.createdDate, 'yyyy-MM-dd')}</span>
         </div>
         <div>{post.content}</div>
       </article>
