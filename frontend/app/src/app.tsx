3aa5dcba9aaae50e6c29620fc44edef30945e0bd
arlechann <dragnov3728@gmail.com>
2022-05-26 22:22:24 +0900

    Use Route API

--- frontend/app/src/app.tsx
+++ frontend/app/src/app.tsx
@@ -1,5 +1,5 @@
 import { FunctionComponent, render } from 'preact';
-import Router from 'preact-router';
+import { Route, Router } from 'preact-router';
 
 import { Header } from './components/header';
 import { IndexPage } from './pages';
@@ -13,12 +13,15 @@ const App: FunctionComponent = () => {
     <>
       <Header />
       <Router>
-        <IndexPage path='/' />
-        <PostListPage path='/posts' />
+        <Route path='/' component={IndexPage} />
+        <Route path='/posts' component={PostListPage} />
         <div default><h1>404 Not Found</h1></div>
       </Router>
     </>
   );
 };
+
+const root = document.getElementById('app');
+if (root == null) { throw new Error; }
 
 render(<App />, root);
