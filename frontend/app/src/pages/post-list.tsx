b40894039a0ec70d4db40bc8304dd41a34682968
arlechann <dragnov3728@gmail.com>
2022-05-30 01:02:29 +0900

    Change to call APIs

--- frontend/app/src/pages/post-list.tsx
+++ frontend/app/src/pages/post-list.tsx
@@ -1,21 +1,38 @@
 import { FunctionComponent } from 'preact';
+import { useEffect, useState } from 'preact/hooks';
 import { PageProps } from '../types/page';
-
 import { PostList } from '../components/post-list';
+
+import { config } from '../config';
+import { Post } from '../types/post';
 
 type PostListPageProps = PageProps;
 
 export const PostListPage: FunctionComponent<PostListPageProps> = (props: PostListPageProps) => {
-  const post = {
-    slug: 'slug',
-    title: '記事タイトル',
-    content: '記事本文',
-    createdDate: new Date,
-    lastUpdatedDate: new Date,
-  };
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    fetch(config.API_URL + '/posts', {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        accept: 'application/json',
+      },
+    })
+      .then(res => {
+        if (!res.ok) { throw new Error(res.statusText); }
+        return res.json();
+      })
+      .then(json => {
+        console.debug(json);
+        setPosts(json);
+      })
+      .catch(err => console.error(err));
+  }, []);
+
   return (
     <main>
-      <PostList posts={[post, post, post]} />
+      <PostList posts={posts} />
     </main>
   );
 };
--- frontend/app/src/pages/post.tsx
+++ frontend/app/src/pages/post.tsx
@@ -1,34 +1,60 @@
 import { FunctionComponent } from 'preact';
-import { format } from 'date-fns';
+import { useEffect, useState } from 'preact/hooks';
+import { format, parse } from 'date-fns';
 import { PageProps } from '../types/page';
 import { Post } from '../types/post';
+import { config } from '../config';
 
 type PostPageProps = PageProps & {
   slug: string;
 };
 
 export const PostPage: FunctionComponent<PostPageProps> = (props: PostPageProps) => {
-  const post: Post = {
-    slug: 'slug',
-    title: '記事タイトル',
-    content: '記事本文',
-    createdDate: new Date,
-    lastUpdatedDate: new Date,
-  };
+  const [post, setPost] = useState<Post | null>(null);
+
+  useEffect(() => {
+    fetch(config.API_URL + `/posts/${props.slug}`, {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+      },
+    })
+      .then(res => {
+        if (!res.ok) { throw new Error(res.statusText); }
+        return res.json();
+      })
+      .then(json => {
+        console.debug(json);
+        setPost({
+          slug: json.slug,
+          title: json.title,
+          content: json.content,
+          createdDate: parse(json.created_at, 'yyyy-MM-dd HH:mm:ss XXXX', Date.now()),
+          lastUpdatedDate: parse(json.last_updated_at, 'yyyy-MM-dd HH:mm:ss XXXX', Date.now()),
+        });
+      })
+      .catch(err => console.error(err));
+  }, []);
+
   return (
     <main>
-      <article>
-        <header>
-          <h1>{post.title}</h1>
-          <section>
-            Updated: <time>{format(post.lastUpdatedDate, 'yyyy-MM-dd')}</time>,
-            Posted: <time>{format(post.createdDate, 'yyyy-MM-dd')}</time>
-          </section>
-        </header>
-        <section>
-          {post.content}
-        </section>
-      </article>
+      {
+        post == null
+          ? ''
+          :
+          <article>
+            <header>
+              <h1>{post.title}</h1>
+              <section>
+                Updated: <time>{format(post.lastUpdatedDate, 'yyyy-MM-dd')}</time>,
+                Posted: <time>{format(post.createdDate, 'yyyy-MM-dd')}</time>
+              </section>
+            </header>
+            <section>
+              {post.content}
+            </section>
+          </article>
+      }
     </main>
   );
 };
